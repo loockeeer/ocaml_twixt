92368111ae5072caa6097ddbfd5b79c8077a558e
TheoFj <theo.feijoo@gmail.com>
2024-09-22 17:23:59 +0200

    collision detection

--- lib/twixt.ml
+++ lib/twixt.ml
@@ -18,7 +18,8 @@ let board_create size =
 let allowed_movements = [|( 2, 1); (1,2); (-1,2); (-2,1); (-2,-1); (-1,-2); (1,-2); (2,-1)|] |> Array.map (fun x -> {line=fst x; col=snd x});;
 
 let add_pos p1 p2 =
-    {line=p1.line+p2.line;col=p1.line+p2.line};;
+  {line=p1.line+p2.line;
+   col=p1.col+p2.col};;
 
 let check_pos board p =
     let a, b = p.line, p.col in
@@ -74,10 +75,73 @@ let check_victory board =
     with FastExit x -> (true, x);;
 
 (* TODO *)
-let check_collision board c1 c2 = false;;
+
+let transfo i =
+  match i with
+  |0 -> (fun p -> (fst p, snd p))
+  |1 -> (fun p -> (-fst p, snd p))
+  |2 -> (fun p -> (-snd p, fst p))
+  |3 -> (fun p -> (-snd p, -fst p))
+  |4 -> (fun p -> (-fst p, -snd p))
+  |5 -> (fun p -> (fst p, -snd p))
+  |6 -> (fun p -> (snd p, -fst p))
+  |7 -> (fun p -> (snd p, fst p))
+  |_ -> failwith "chef???"
+;;
+
+let transfo i =
+  match i with
+  |0 -> (fun p -> (fst p, snd p))
+  |1 -> (fun p -> (-fst p, snd p))
+  |2 -> (fun p -> (-snd p, fst p))
+  |3 -> (fun p -> (-snd p, -fst p))
+  |4 -> (fun p -> (-fst p, -snd p))
+  |5 -> (fun p -> (fst p, -snd p))
+  |6 -> (fun p -> (snd p, -fst p))
+  |7 -> (fun p -> (snd p, fst p))
+  |_ -> failwith "chef???"
+;;
+
+let index link_type = (*ya plus court mais nsm*)
+  match link_type with
+  |(2,1)   -> 0
+  |(2,-1)  -> 1
+  |(1,-2)  -> 2
+  |(-1,-2) -> 3
+  |(-2,-1) -> 4
+  |(-2,1)  -> 5
+  |(-1,2)  -> 6
+  |(1,2)   -> 7
+  |_ -> failwith "frerot??"
+;;
+
+let possible_collisions i =
+  Array.map (fun x -> ((transfo i) (fst x), index ((transfo i) (snd x))))
+  [|((1,-1),(-1,2));
+    ((1,0), (-1,2));
+    ((2,-1),(-1,2));
+    ((2,0),(-1,2));
+    ((1,0),(-2,1));
+    ((2,0),(-2,1));
+    ((3,0),(-2,1));
+    ((0,-1),(1,2));
+    ((1,0),(1,2))|]
+;;
+
+let check_collision board c1 i =
+  let t = possible_collisions i in
+  let x, y = c1.pos.line, c1.pos.col in
+  let output = ref false in
+  for n=0 to 8 do
+    let p, j = t.(n) in
+    let c = board.cells.(x+fst p).(y+snd p) in
+    if c.links.(j) then output := true
+  done;
+  !output
+;;
 
 let can_link board c1 c2 i =
-    c1.owner = c2.owner && c1.owner <> Unowned && not c1.links.(i) && not (check_collision board c1 c2);; 
+    c1.owner = c2.owner && c1.owner <> Unowned && not c1.links.(i) && not (check_collision board c1 i);; 
 
 let get_available_links board cell =
     get_near_cells board cell |> Array.mapi (fun i c -> 
